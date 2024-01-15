c89d6559c136271da071f6678691d9c9b345ce54
Andrej Naňo <nano@andrejnano.sk>
2024-01-15 13:16:52 +0100

    fix author

--- src/app/(web)/blog/[slug]/Author.tsx
+++ src/app/(web)/blog/[slug]/Author.tsx
@@ -7,8 +7,8 @@ dayjs.locale('sk')
 
 export const BlogPostAuthor = ({ post }: { post: PostType }) => {
   return (
-    <div className="flex flex-row items-center gap-4">
-      <div className="h-16 w-16 overflow-clip rounded-full bg-brand opacity-90 filter">
+    <div className="flex flex-row items-center gap-4 px-4">
+      {/* <div className="h-16 w-16 overflow-clip rounded-full bg-brand opacity-90 filter">
         <div className="aspect-h-1 aspect-w-1 relative overflow-hidden bg-brand">
           <Image
             alt={post.author?.image?.alt || 'Placeholder image'}
@@ -17,7 +17,7 @@ export const BlogPostAuthor = ({ post }: { post: PostType }) => {
             className="object-cover object-center duration-700 ease-in-out group-hover:opacity-75"
           />
         </div>
-      </div>
+      </div> */}
       <div className="flex flex-col text-left">
         <span className="text-lg font-medium text-white">{post.author?.name}</span>
         <div className="text-sm text-neutral-200">{dayjs(post.publishedAt).format('DD. MMMM YYYY')}</div>
