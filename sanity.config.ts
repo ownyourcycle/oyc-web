afacc726f24aa8f57f4b73e542a50c600045c6d0
Andrej Naňo <nano@andrejnano.sk>
2024-01-15 16:50:59 +0100

    update sanity studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,7 +4,7 @@
 
 import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
-import { deskTool } from 'sanity/desk'
+import { structureTool } from 'sanity/structure'
 import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
@@ -18,7 +18,7 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
-    deskTool(),
+    structureTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     vercelDeployTool(),
